0c0b272a6caa5b5dbd777fdf3ccce04b6095ae67
Roger Tuan <r.tuan@datocms.com>
2025-09-30 11:30:00 -0700

    Change example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
     "data": {
       "upload": {
         "responsiveImage": {
-          "sizes": "(max-width: 1024px) 100vw, 1024px",
-          "srcSet": "https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=0.25 256w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=0.5 512w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=0.75 768w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg 1024w",
-          "src": "https://www.datocms-assets.com/174347/1759255105-example-image.jpg",
+          "sizes": "(max-width: 500px) 100vw, 500px",
+          "srcSet": "https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=0.25&h=500&w=500 125w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=0.5&h=500&w=500 250w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=0.75&h=500&w=500 375w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=2&h=500&w=500 500w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=1.5&h=500&w=500 750w,https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=2&h=500&w=500 1000w",
+          "src": "https://www.datocms-assets.com/174347/1759255105-example-image.jpg?dpr=2&h=500&w=500",
           "aspectRatio": 1,
           "base64": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBg4ICAgVDw0LDhEPDQ0NDhkNDQ0SFxMZGBYTFhUmHysjGh0oKSEWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLDg0OHA0NHS8cFhwzLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIABgAGAMBIgACEQEDEQH/xAAZAAACAwEAAAAAAAAAAAAAAAAABAMFBgH/xAAeEAACAgEFAQAAAAAAAAAAAAAAAwECEQQFEhUxIf/EABUBAQEAAAAAAAAAAAAAAAAAAAQF/8QAGREAAgMBAAAAAAAAAAAAAAAAAAIBAyIS/9oADAMBAAIRAxEAPwDS2fNVfIFq662cYH9OurF4mCbrlRHLBHuSOinU2RaXclZmDgxqFUorEQAutY5DO2ivpu6l+DvcLsoABXzoXVGSFu5LusAAUkzyGeNH/9k=",
-          "height": 1024,
-          "width": 1024,
+          "height": 500,
+          "width": 500,
           "title": null
         }
       }
@@ -29,13 +29,18 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>DatoCMS Next.js and react-datocms image tests</h1>
-        <h2>Example 1: With our Image component</h2>
-        <p>This example shows how our image component is typically used.</p>
-        <DatoImage data={responsiveImage}/>
 
-        <h2>Example 2: With the Next.js Image component (not ours)</h2>
-        <p>This example shows how you'd use our GraphQL response with Next's own image component.</p>
-        <NextImage src={responsiveImage.src} width={responsiveImage.width} height={responsiveImage.height} alt={''}/>
+        <h2>Everything below is within a noscript tag</h2>
+        <noscript>
+          <h3>As an image SRC</h3>
+          <img src={responsiveImage.src} alt="" />
+
+          <h3>As string</h3>
+          <p>{responsiveImage.src}</p>
+
+          <h3>As a data attribute</h3>
+          <div data-test={responsiveImage.src}>As data attribute (inspect this dev)</div>
+        </noscript>
       </main>
     </div>
   );
